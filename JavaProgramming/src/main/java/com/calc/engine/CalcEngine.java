8528d0c07f61476a63b3e15209b259d34078fc62
Philip-Kenya <Phlpsnjug@gmail.com>
2021-09-18 00:08:29 +0300

    command-line arguments

--- JavaProgramming/src/main/java/com/calc/engine/CalcEngine.java
+++ JavaProgramming/src/main/java/com/calc/engine/CalcEngine.java
@@ -4,7 +4,7 @@ public class CalcEngine {
 
 	public static void main(String[] args) {
 		double[] leftVals= {
-				100.0d, 25.0d,225.0d, 11.0d	
+				200.0d, 25.0d,225.0d, 11.0d	
 		};
 		double[] rightVals= {
 				50.0d, 92.0d, 17.0d, 3.0d
@@ -13,7 +13,9 @@ public class CalcEngine {
 				'd', 'a', 's','m'
 		};
         double [] results = new double[opCodes.length];
-        
+        if (args.length==0) {
+			
+		
 		for(int i =0 ; i<opCodes.length; i++)
 		{
 			results[i]= execute(opCodes[i], leftVals[i], rightVals[i]);
@@ -21,7 +23,24 @@ public class CalcEngine {
 		
 		for(double currentResult: results)
 				
-		System.out.println(currentResult);
+		System.out.println(currentResult);}
+        
+        
+        else if (args.length==3) {
+        	handleCommandLine(args);
+			
+		}
+        else {
+        	System.out.println("please provide an operation and 2 numeric values");
+        }
+		
+	}
+		private static void handleCommandLine(String[] args) {
+		char opCode=args[0].charAt(0);
+		double leftVal= Double.parseDouble(args[1]);
+		double rightVal= Double.parseDouble(args[2]);
+		double result= execute(opCode, leftVal, rightVal);
+		System.out.println(result);
 		
 	}
 		static double execute(char opCode, double leftVal, double rightVal)
